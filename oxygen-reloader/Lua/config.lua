824573f423f1361f29f73cee11b7539a898b847e
Vladimir Kammerer <Zipliks@gmail.com>
2023-04-06 02:37:10 +0300

    refactor: uppercased since all variables are const

--- oxygen-reloader/Lua/config.lua
+++ oxygen-reloader/Lua/config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.version = "1.0.1"
-Config.button = Keys.Q -- https://docs.monogame.net/api/Microsoft.Xna.Framework.Input.Keys.html
-Config.min_swap_condition = 50 -- Minimal item condition to be swapped
-Config.min_suitable_condition = 90 -- Minimal item condition to be treated as variant
+Config.VERSION = "1.0.2"
+Config.BUTTON = Keys.Q -- https://docs.monogame.net/api/Microsoft.Xna.Framework.Input.Keys.html
+Config.MIN_SWAP_CONDITION = 50 -- Minimal item condition to be swapped
+Config.MIN_SUITABLE_CONDITION = 90 -- Minimal item condition to be treated as variant
