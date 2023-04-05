5c1c7d6ec100b231418655f27142a08c2559d715
Vladimir Kammerer <Zipliks@gmail.com>
2023-04-06 02:35:41 +0300

    refactor: general code improvements
    * Deleted redundant `ReplaceMaskTank` function since it's better to use only one for both gear types
    * Added better `nil` check
    * Added `return` after `ReplaceAny` function call to prevent further validation
    * Reduced the number of if statements and combined conditions to improve readability

--- oxygen-reloader/Lua/ForcedAutorun/main.lua
+++ oxygen-reloader/Lua/ForcedAutorun/main.lua
@@ -3,71 +3,60 @@ require("config")
 
 print(
     "--------\n" ..
-    "Running Oxygen Reloader version: " .. Config.version ..
-    "\nCurrent reload button is: " .. tostring(Config.button) ..
+    "Running Oxygen Reloader version: " .. Config.VERSION ..
+    "\nCurrent reload button is: " .. tostring(Config.BUTTON) ..
     "\n--------"
 )
 
 Hook.Add("keyUpdate", "AutoReplaceOxygenTank", function(keyargs)
-    if not PlayerInput.KeyDown(Config.button) then
-        return
-    end
-    
-    if Character.DisableControls then
-        return
-    end
-    if Character.Controlled == nil or Character.Controlled.Inventory == nil then
+    if not PlayerInput.KeyDown(Config.BUTTON) or Character.DisableControls then
         return
     end
 
     local character = Character.Controlled
+    if not character or not character.Inventory then
+        return
+    end
+
     local suitSlot = character.Inventory.GetItemInLimbSlot(InvSlotType.OuterClothes)
     local headSlot = character.Inventory.GetItemInLimbSlot(InvSlotType.Head)
 
     if suitSlot ~= nil and (suitSlot.HasTag("diving") or suitSlot.HasTag("deepdiving")) then
-        ReplaceSuitTank(character, suitSlot)
+        ReplaceTank(character, suitSlot)
     elseif headSlot ~= nil and headSlot.HasTag("diving") then
-        ReplaceMaskTank(character, headSlot)
+        ReplaceTank(character, headSlot)
     end
 end)
 
 
--- Used if there is no tank in diving suit
-function ReplaceAny(character, diving_gear)
+-- Used if there is no tank in diving gear
+function ReplaceAny(character, gearSlot)
     local variants = CollectAllTanks(character)
-    if #variants == 0 or variants == nil then
+    if not variants or #variants == 0 then
         return
     end
 
     -- Find the best quality
     local max_quality = FindMaxQuality(variants)
+    variants = RemoveLowQuality(variants, max_quality)
 
-    -- Remove all tanks of lower quality
-    local i = 1
-    while i <= #variants do
-        if variants[i].Quality < max_quality then
-            table.remove(variants, i)
-        else
-            i = i + 1
-        end
-    end
-
-    local best_tank = FindBiggestCondition(variants)
-    diving_gear.OwnInventory.TryPutItem(best_tank, 0, true, false, character)
+    local bestTank = FindBiggestCondition(variants)
+    gearSlot.OwnInventory.TryPutItem(bestTank, 0, true, false, character)
 end
 
 -- Replace for better tank
-function ReplaceSuitTank(character, suit_slot)
-    local suit_tank = suit_slot.OwnInventory.FindItemByTag("oxygensource", true)
+function ReplaceTank(character, gearSlot)
+    local gearTank = gearSlot.OwnInventory.FindItemByTag("oxygensource", true)
 
-    if suit_tank == nil then
-        ReplaceAny(character, suit_slot)
-    elseif suit_tank.Condition >= Config.min_swap_condition then
+    if gearTank == nil then
+        ReplaceAny(character, gearSlot)
+        return
+    elseif gearTank.Condition >= Config.MIN_SWAP_CONDITION then
         return
     end
 
     local variants = CollectAllTanks(character)
-    if #variants == 0 or variants == nil then
+    if not variants or #variants == 0 then
         return
     end
 
@@ -75,34 +64,11 @@ function ReplaceSuitTank(character, suit_slot)
     local max_quality = FindMaxQuality(variants)
     variants = RemoveLowQuality(variants, max_quality)
 
+    local bestTank = FindBiggestCondition(variants)
 
-    local best_tank = FindBiggestCondition(variants)
-    if best_tank == suit_tank then
+    -- This one just to prevent annoying animation as if tank had been swapped but in fact it wasn't
+    if bestTank == gearTank then
         return
     end
-    suit_slot.OwnInventory.TryPutItem(best_tank, 0, true, false, character)
-end
-
-function ReplaceMaskTank(character, headSlot)
-    local mask_tank = headSlot.OwnInventory.FindItemByTag("oxygensource", true)
-
-    if mask_tank == nil then
-        ReplaceAny(character, headSlot)
-    end
-
-    local variants = CollectAllTanks(character)
-    if #variants == 0 or variants == nil then
-        return
-    end
-
-    -- Find the best quality
-    local max_quality = FindMaxQuality(variants)
-    variants = RemoveLowQuality(variants, max_quality)
-
-
-    local best_tank = FindBiggestCondition(variants)
-    if best_tank == mask_tank then
-        return
-    end
-    headSlot.OwnInventory.TryPutItem(best_tank, 0, true, false, character)
+    gearSlot.OwnInventory.TryPutItem(bestTank, 0, true, false, character)
 end
